9a64bab54d601bb4f27e725222edf9aea634989e
Dilbar Singh Lamba <notdilbarsl@gmail.com>
2024-09-10 23:15:27 +0530

    Just Changing Settings

---
+++ unused.js
@@ -0,0 +1,16 @@
+function testComplexFunction() {
+  if (true) {
+    if (true) {
+      for (let i = 0; i < 5; i++) {
+        if (true) {
+          switch (i) {
+            case 1:
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
+  }
+}
