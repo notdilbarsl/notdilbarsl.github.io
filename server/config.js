6feb6496b7eb8ce4016bc027c7bce1bc4e09a1c3
Dilbar Singh Lamba <notdilbarsl@gmail.com>
2024-11-14 17:59:42 +0530

    Updated POST request handling

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
 
 const app = express();
 
