b31ae920ef4cc097122dd581bc67751a29c76680
Dilbar Singh Lamba <notdilbarsl@gmail.com>
2024-11-14 16:12:39 +0530

    Load Balancing Testing

--- client/updateLeaderboard.js
+++ client/updateLeaderboard.js
@@ -1,7 +1,7 @@
 // Function to update the leaderboard
 /* global fetch */
 export function updateLeaderboard(gridSize, mineCount) {
-  fetch(`https://notdilbarsl-github-io-2.onrender.com/leaderboard?gridSize=${gridSize}x${gridSize}&mineCount=${mineCount}`)
+  fetch(`https://notdilbarsl-github-io-3.onrender.com/leaderboard?gridSize=${gridSize}x${gridSize}&mineCount=${mineCount}`)
     .then((response) => response.json())
     .then((data) => {
       const leaderboardTableBody = document.querySelector('#leaderboardTable tbody');
